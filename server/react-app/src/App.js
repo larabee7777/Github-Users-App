28ba0281b2422a5246d25aafa3ed1f9b2aa45bdf
larabee7777 <58976112+larabee7777@users.noreply.github.com>
2022-02-02 22:26:40 +0200

    UI formatting

--- server/react-app/src/App.js
+++ server/react-app/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function AppMenu() {
+  return (
+    <div>
+      <div class="topnav">
+        <div class="logo">
+          <h3>Github Users</h3>
+        </div>
+        <div>
+          <a class="active" href="#home">
+            Search Users
+          </a>
+          <a href="#news">Saved Users</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function AppSearch() {
+  return (
+    <div class="SearchQueryContainer">
+      <button type="submit" class="submit-btn">
+        <i class="fa fa-search fa-2x"></i>
+      </button>
+      <input
+        type="text"
+        class="submit-text"
+        placeholder="  username"
+        name="search"
+      ></input>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppMenu />
+      <AppSearch />
     </div>
   );
 }
