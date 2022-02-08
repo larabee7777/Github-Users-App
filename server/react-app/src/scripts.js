eadd97c14112547a2846942f87c11551b41c81ae
larabee7777 <58976112+larabee7777@users.noreply.github.com>
2022-02-08 08:46:14 +0200

    + fetch users page to display working api fetch

--- server/react-app/src/scripts.js
+++ server/react-app/src/scripts.js
@@ -1,67 +1,24 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
+import { DisplaySearchResult } from "./components/searchresults";
 
-var listUsers = [];
-
-export class FetchUsers extends React.Component {
-  // Constructor
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
-
-  // ComponentDidMount is used to
-  // execute the code
-  componentDidMount() {
-    fetch("https://api.github.com/users")
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
-      });
-  }
-  render() {
-    const { DataisLoaded, items } = this.state;
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1> Loading ... </h1>{" "}
-        </div>
-      );
-    items.map((item) => listUsers.push(items));
-    return (
-      <div className="App">
-        <h1> All Users </h1>
-        {/* {items.map((item) => listUsers.push(items))} */}
-        {console.log(listUsers[0][0].login)}
-        <h3>{listUsers[0][0].login}</h3>
-      </div>
-    );
-  }
+function DisplayUsers() {
+  return (
+    <div>
+      <FetchUsers />
+    </div>
+  );
 }
 
-class DisplayUsers extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <FetchUsers />
-      </div>
-    );
-  }
+function DisplaySearchResults() {
+  return (
+    <div>
+      <DisplaySearchResult />
+    </div>
+  );
 }
 
-export default DisplayUsers;
+export { DisplayUsers, DisplaySearchResults };
 
 // function SearchBtn() {
 //   return (
